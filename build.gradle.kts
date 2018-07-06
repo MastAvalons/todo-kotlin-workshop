d5d146f35f706844f1e81286f7d0de5febb99edc
Pavlo Stavytskyi <pavlo_stavytskyi@epam.com>
2018-07-07 00:44:05 +0300

    added gradle kotlin dsl

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,17 +1,17 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 
 buildscript {
-    ext.kotlin_version = '1.2.50'
+    val kotlinVersion: String by extra { "1.2.50" }
     repositories {
         google()
         jcenter()
     }
     dependencies {
-        classpath 'com.android.tools.build:gradle:3.1.3'
-        classpath "org.jetbrains.kotlin:kotlin-gradle-plugin:$kotlin_version"
+        classpath("com.android.tools.build:gradle:3.1.3")
+        classpath(kotlin("gradle-plugin", version = kotlinVersion))
 
         // NOTE: Do not place your application dependencies here; they belong
-        // in the individual module build.gradle files
+        // in the individual module build.gradle.kts files
     }
 }
 
@@ -22,8 +22,7 @@ allprojects {
     }
 }
 
-task clean(type: Delete) {
-    delete rootProject.buildDir
+task<Delete>("clean") {
+    delete(rootProject.buildDir)
 }
-
 
